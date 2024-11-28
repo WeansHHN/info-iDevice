2ad67540a747922e1985f69ca317ad3ca5f14e29
Weans <137310130+WeansHHN@users.noreply.github.com>
2024-11-28 22:54:35 +0700

    Update Demo.mm

--- Demo.mm
+++ Demo.mm
@@ -4,6 +4,7 @@
 #include <mach/mach.h>
 #include <sys/sysctl.h>
 #include <sys/stat.h>
+#import "iDecive.h"
 
 bool isJailbroken() {
     //Rootfull
@@ -39,76 +40,83 @@ bool isJailbroken() {
     return false;
 }
 
+UIDevice *device = [UIDevice currentDevice];
+    device.batteryMonitoringEnabled = YES;
 
+    // Lấy thông tin về pin
+    float batteryLevel = device.batteryLevel * 100;
+    NSString *chargingStatus;
+    if (device.batteryState == UIDeviceBatteryStateCharging) {
+        chargingStatus = @"Đang Sạc";
+    } else if (device.batteryState == UIDeviceBatteryStateFull) {
+        chargingStatus = @"Đầy Pin";
+    } else {
+        chargingStatus = @"Đã Ngắt Sạc";
+    }
 
-#import "iDevice.h"
+    // Lấy thông tin ngày
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"EEEE dd/MM/yyyy"];
+    NSString *dateString = [dateFormatter stringFromDate:[NSDate date]];
 
-if (ImGui::BeginTabItem(ICON_FA_USER" Thông tin"))
-{               
-ImGui::TextColored(ImColor(255, 255, 255), "Hello ");
-ImGui::SameLine();
-ImGui::TextColored(ImColor(255, 255, 255), "%s", namedv.c_str());
+    // Lấy số nhân CPU
+    long num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
 
-ImGui::TextColored(ImVec4(1.0f, 1.0f, 1.0f, 1.0f), "Trạng Thái: ");
-ImGui::SameLine();
-if (isJailbroken()) {
-    ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "Đã Jailbreak !");
-} else {
-    ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "Chưa Jailbreak !");
-}
-ImGui::Separator();
+    // Tính toán RAM
+    size_t len;
+    natural_t free_ram = [[NSProcessInfo processInfo] physicalMemory] / 1024 / 1024; // MB
+    natural_t used_ram = 0;  // Tính RAM đã sử dụng (nếu có logic thực hiện)
 
-ImVec4 red = ImVec4(0.92f, 0.59f, 0.58f, 1.0f);
-ImVec4 green = ImVec4(0.76f, 0.88f, 0.77f, 1.0f);
-ImVec4 cyan = ImVec4(0.0f, 1.0f, 1.0f, 1.0f);
-ImVec4 blue = ImVec4(0.77f, 0.87f, 0.96f, 1.00f);
-ImVec4 white = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+    // Lấy tên thiết bị
+    std::string namedv = [[device name] UTF8String];  // Chuyển NSString thành std::string
 
-ImGui::TextColored(white, "Ngày: ");
-ImGui::SameLine();
-ImGui::TextColored(blue, "%s", [dateString UTF8String]);
+if (currentTab == 0) {
 
-ImGui::TextColored(white, "Pin: ");
-ImGui::SameLine();
-ImGui::TextColored(cyan, "%.0f%%", batteryLevel);
-ImGui::SameLine();
-ImGui::TextColored(white, " / ");
-ImGui::SameLine();
-if (device.batteryState == UIDeviceBatteryStateCharging) {
-    ImGui::TextColored(green, "Đang Sạc");
-} else if (device.batteryState == UIDeviceBatteryStateFull) {
-    ImGui::TextColored(green, "Đầy Pin");
-} else {
-    ImGui::TextColored(red, "Đã Ngắt Sạc");
-}
-
-ImGui::TextColored(white, "Tổng CPU:");
-ImGui::SameLine();
-ImGui::TextColored(ImColor(0, 255, 0), " %ld bytes", ram_total);
-ImGui::SameLine();
-ImGui::TextColored(white, " / ");
-ImGui::SameLine();
-ImGui::TextColored(ImColor(102, 255, 102), "Nhân: %ld", num_cpus);
-
-ImVec4 used_text_color(1, 0, 1, 1);
-ImVec4 used_info_color(1, 1, 0, 1);
-
-ImGui::TextColored(white, "Sử dụng RAM:");
-ImGui::SameLine();
-int used_ram_len = snprintf(used_ram_str, sizeof(used_ram_str), "%s: %d MB", "Đã sử dụng", used_ram);
-
-if (used_ram_len > 0) {
-    ImGui::TextColored(used_text_color, "%s", "Đã sử dụng:");
+    // Hiển thị thông tin trên ImGui
+    ImGui::TextColored(ImColor(255, 255, 255), "Hello");
     ImGui::SameLine();
-    ImGui::TextColored(used_info_color, "%d MB", used_ram);
-}
+    ImGui::TextColored(ImColor(255, 255, 255), "%s", namedv.c_str());
 
-ImGui::SameLine();
-ImGui::TextColored(white, " / ");
-ImGui::SameLine();
-sprintf(free_ram_str, " %d MB", free_ram);
-if (strlen(free_ram_str) > 0) {
-    ImGui::TextColored(ImVec4(0, 1, 0, 1), "RAM trống: ");
+    ImGui::TextColored(ImVec4(1.0f, 1.0f, 1.0f, 1.0f), "Trạng Thái:");
     ImGui::SameLine();
-    ImGui::TextColored(ImVec4(1, 0, 0, 1), "%s", free_ram_str);
+    if (isJailbroken()) {  // Kiểm tra jailbreak của thiết bị
+        ImGui::TextColored(ImVec4(1.0f, 0.0f, 0.0f, 1.0f), "Đã Jailbreak !");
+    } else {
+        ImGui::TextColored(ImVec4(0.0f, 1.0f, 0.0f, 1.0f), "Chưa Jailbreak !");
+    }
+
+    ImGui::Separator();
+
+    // Màu sắc
+    ImVec4 red = ImVec4(0.92f, 0.59f, 0.58f, 1.0f);
+    ImVec4 green = ImVec4(0.76f, 0.88f, 0.77f, 1.0f);
+    ImVec4 cyan = ImVec4(0.0f, 1.0f, 1.0f, 1.0f);
+    ImVec4 blue = ImVec4(0.77f, 0.87f, 0.96f, 1.00f);
+    ImVec4 white = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+
+    // Hiển thị ngày
+    ImGui::TextColored(white, "Ngày:");
+    ImGui::SameLine();
+    ImGui::TextColored(blue, "%s", [dateString UTF8String]);
+
+    // Hiển thị trạng thái pin
+    ImGui::TextColored(white, "Pin:");
+    ImGui::SameLine();
+    ImGui::TextColored(cyan, "%.0f%%", batteryLevel);
+    ImGui::SameLine();
+    ImGui::TextColored(white, "/");
+    ImGui::SameLine();
+    ImGui::TextColored((device.batteryState == UIDeviceBatteryStateCharging || device.batteryState == UIDeviceBatteryStateFull) ? green : red,
+                       "%s", [chargingStatus UTF8String]);
+
+    // Hiển thị CPU
+    ImGui::TextColored(white, "Tổng CPU:");
+    ImGui::SameLine();
+    ImGui::TextColored(ImColor(0, 255, 0), " %ld", num_cpus);
+
+    // Hiển thị RAM
+    ImGui::TextColored(white, "RAM trống:");
+    ImGui::SameLine();
+    ImGui::TextColored(ImColor(0, 255, 0), "%u MB", free_ram);
+
 }
